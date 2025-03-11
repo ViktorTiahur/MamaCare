615bf199fe0da4b2d3f9d65ee50fb1678b9074a5
Viktor Tiahur <abik2002@ukr.net>
2025-03-11 18:38:55 +0100

    header tsblet 11.03

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,26 +1,27 @@
-import React { ReactNode } from "react";
-import './Modal.module.scss';
+import React, { ReactNode } from "react";
+import "./Modal.module.scss";
 
 interface ModalProps {
-    show: boolean;
-    onClose: ()=> void;
-    children: ReactNode;
+  show: boolean;
+  onClose: () => void;
+  children: ReactNode;
 }
 
-const Modal: React:FC:<ModalProps> = ({show,onClose,children})=>{
-    if(!show){
-        return null;
-    }
+const Modal: React.FC<ModalProps> = ({ show, onClose, children }) => {
+  if (!show) {
+    return null;
+  }
 
-    return (
-        <div className="modal">
-            <div className="modal-content">
-                <button className='close-button' onClick={onClose}>x</button>
-                {children}
-            </div>
-        </div>
-    );
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <button className="close-button" onClick={onClose}>
+          x
+        </button>
+        {children}
+      </div>
+    </div>
+  );
 };
-
 
 export default Modal;
